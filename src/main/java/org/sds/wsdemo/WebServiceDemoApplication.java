1d57612eb307f4fbbd3d92117fb5d30fd5c7733c
ankit <ankit.kumar@seamless.se>
2021-06-20 10:24:55 +0530

    Modified: The Application name

--- src/main/java/org/sds/wsdemo/WebServiceDemoApplication.java
+++ src/main/java/org/sds/wsdemo/WebServiceDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WsDemoApplication {
+public class WebServiceDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WsDemoApplication.class, args);
+        SpringApplication.run(WebServiceDemoApplication.class, args);
     }
 
 }
