bfae872a3a13ae9bbbba6eff876d1b362ff02268
ankit <ankit.kumar@seamless.se>
2021-06-22 22:09:44 +0530

    Modified: The function signature of AppService interface

--- src/main/java/org/sds/wsdemo/service/AppService.java
+++ src/main/java/org/sds/wsdemo/service/AppService.java
@@ -4,21 +4,30 @@ import org.sds.wsdemo.soap.apis.dtos.add.AdditionRequest;
 import org.sds.wsdemo.soap.apis.dtos.divide.DivideRequest;
 import org.sds.wsdemo.soap.apis.dtos.multiply.MultiplyRequest;
 import org.sds.wsdemo.soap.apis.dtos.subtract.SubtractRequest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/calculate")
 public interface AppService {
 
-    @GetMapping("/addition")
+    @GetMapping(value = "/addition",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     String addition(AdditionRequest request);
 
-    @GetMapping("/subtraction")
+    @GetMapping(value = "/subtraction",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     String subtraction(SubtractRequest request);
 
-    @GetMapping("/multiplication")
+    @GetMapping(value = "/multiplication",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     String multiplication(MultiplyRequest request);
 
-    @GetMapping("/division")
+    @GetMapping(value = "/division",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     String division(DivideRequest request);
 }
